83d5b21a43aa1035345375f675dcf2151d16abc3
timecorner35 <71405627+timecorner35@users.noreply.github.com>
2020-09-24 14:00:25 +0300

    Update Question6.java

--- assignmentThree/Question6.java
+++ assignmentThree/Question6.java
@@ -8,7 +8,7 @@ public class Question6 {
 		final int lethal=10000;
 		int NumberofMiligramsinDrink=500;
 		System.out.println("NumberofMiligramsinDrink: "+NumberofMiligramsinDrink);
-		System.out.println("It would take about "+(int)(lethal/NumberofMiligramsinDrink)+" drink for an overdose");
+		System.out.println("It would take about "+(lethal/NumberofMiligramsinDrink)+" drink for an overdose");
 		
 		
 				
