aefff948d1d48b81d49d56754cb3ecc11adf78ad
timecorner35 <71405627+timecorner35@users.noreply.github.com>
2020-09-26 17:06:17 +0300

    Update Question7.java

--- assignmentThree/Question7.java
+++ assignmentThree/Question7.java
@@ -7,10 +7,13 @@ public class Question7 {
 		//quarter=25c, dime=10c, nickle=5p, payment=100c,
 		
 		int payment=100;
-		int price=95;
-		int quarters=(payment-price)/25;
-		int dimes=((payment-price-(25*quarters))/10); 
-		int nickles=((payment-price-(25*quarters)-(10*dimes))/5);
+		int price=65;
+		int change= payment-price;
+		int quarters=change/25;
+		change=change%25;
+		int dimes=change/10;
+		change=change%10;
+		int nickles=(change/5);
 		
 		System.out.println("Price in cents: "+price);
 		System.out.println("Your change is: "+quarters+" quarters, "+dimes+" dimes and "+nickles+" nickles");
