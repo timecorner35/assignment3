ad3038e250e6dad05ddbadcb7aec1c183692ecac
timecorner35 <71405627+timecorner35@users.noreply.github.com>
2020-09-26 16:56:22 +0300

    Update Question4.java

--- assignmentThree/Question4.java
+++ assignmentThree/Question4.java
@@ -4,14 +4,15 @@ public class Question4 {
 
 	public static void main(String[] args) {
 		
-		int num1,num2;
-		num1=30;
-		num2=40;
-		num1=40;
-		num2=30;
+		int num1=30,num2=40;
+		num1=num1+num2;
+		num2=num1-num2;
+		num1=num1-num2;
+		
 		
 		System.out.println("num1="+num1);
 		System.out.println("num2="+num2);
+	}
 	}
 
 }
